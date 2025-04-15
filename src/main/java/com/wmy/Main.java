e7fcd84a80c9b75c6f0a3621e7423b7339ea1b49
SC8803 <mengyao.wang@wbstar.com>
2025-04-15 10:37:04 +0800

    init

---
+++ src/main/java/com/wmy/Main.java
@@ -0,0 +1,13 @@
+package com.wmy;
+
+/**
+ * ${description}
+ *
+ * @author wangmengyao
+ * @Date 2025/4/15 10:31
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
