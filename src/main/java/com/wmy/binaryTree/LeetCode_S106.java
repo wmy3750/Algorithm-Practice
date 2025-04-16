44b2b41424b5806c2dab4bcd8bee908095a2a72a
SC8803 <mengyao.wang@wbstar.com>
2025-04-16 10:52:41 +0800

    fix:leetcode-106. 注释修改

--- src/main/java/com/wmy/binaryTree/LeetCode_S106.java
+++ src/main/java/com/wmy/binaryTree/LeetCode_S106.java
@@ -12,22 +12,13 @@ import java.util.Map;
  */
 
 /*
- * 这题一上来你就知道了思路，但是根本不会写出来
- * 1. 知道是分解成子问题，动态规划的思路
- * 2. 确定递归的终止条件，以及递归的子问题
- * 3. 确定递归的子问题，以及子问题的终止条件
- * 4. 确定递归的返回值，以及递归的返回条件
- * 5. 确定递归的顺序，以及递归的顺序
- */
-
-/*
- * 确定中序遍历左孩子范围是 inStart ~ inRootIndex - 1
- * 确定中序遍历右孩子范围是 inRootIndex + 1 ~ inEnd
  * 确定后序遍历左孩子范围是 postStart ~ postStart + x - 1
- * 确定后序遍历右孩子范围是 postStart + x ~ postRootIndex - 1
+ * 确定中序遍历左孩子范围是 inStart ~ inRootIndex - 1
+ * 确定后序遍历右孩子范围是 postStart + x ~ postEnd - 1
+ * 确定中序遍历右孩子范围是 inRootIndex + 1 ~ inEnd
  * 状态方程确认一半了
  * 左树状态方程是 build(inorder, inStart, inRootIndex - 1, postorder, postStart, postStart + x - 1)
- * 右树树状态方程是 build(inorder, inRootIndex + 1, inEnd, postorder, postStart + x, postRootIndex - 1)
+ * 右树树状态方程是 build(inorder, inRootIndex + 1, inEnd, postorder, postStart + x, postEnd - 1)
  * x如何去确认？
  * 假设左树节点右x个，那么在中序遍历中，x可得出，x = inRootIndex - inStart
  * 所以
