ecdfc8ba113929991ca6709d9ff3ef14b0b04f67
SC8803 <mengyao.wang@wbstar.com>
2025-04-17 15:44:01 +0800

    fix:leetcode-700. 二叉搜索树中的搜索

---
+++ src/main/java/com/wmy/binaryTree/bst/LeetCode_S700.java
@@ -0,0 +1,21 @@
+package com.wmy.binaryTree.bst;
+
+import com.wmy.TreeNode;
+import com.wmy.TreeNodeOperate;
+
+/**
+ * @author wangmengyao
+ * @Date 2025/4/16 14:27
+ */
+
+public class LeetCode_S700 {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        if (root.val == val) return root;
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
