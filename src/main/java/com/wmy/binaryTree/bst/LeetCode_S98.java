867bf4492905f4c5665bf08c89b6e9eb12e7abe0
SC8803 <mengyao.wang@wbstar.com>
2025-04-18 17:05:32 +0800

    fix:leetcode-98. 验证二叉搜索树，自己能看懂且会的解法

--- src/main/java/com/wmy/binaryTree/bst/LeetCode_S98.java
+++ src/main/java/com/wmy/binaryTree/bst/LeetCode_S98.java
@@ -19,24 +19,46 @@ public class LeetCode_S98 {
     }
 
     // 定义：该函数返回 root 为根的子树的所有节点是否满足 max.val > root.val > min.val
-    public boolean _isValidBST(TreeNode root, TreeNode min, TreeNode max) {
+    public boolean _isValidBST(TreeNode root, Integer min, Integer max) {
         // base case
         if (root == null) return true;
         // 若 root.val 不符合 max 和 min 的限制，说明不是合法 BST
-        if (min != null && root.val <= min.val) return false;
-        if (max != null && root.val >= max.val) return false;
+        if (min != null && root.val <= min) return false;
+        if (max != null && root.val >= max) return false;
         // 根据定义，限定左子树的最大值是 root.val，右子树的最小值是 root.val
-        return _isValidBST(root.left, min, root)
-                && _isValidBST(root.right, root, max);
+        return _isValidBST(root.left, min, root.val)
+                && _isValidBST(root.right, root.val, max);
     }
 
+
+    public boolean isValidBST1(TreeNode root) {
+        long[] res = _isValid(root);
+        return res[0] == 1;
+    }
+
+    // 数组[是否BST, 树的最小值, 树的最大值]
+    // 第二个元素标识
+    public long[] _isValid(TreeNode root) {
+        if (root == null) return new long[] {1, Long.MAX_VALUE, Long.MIN_VALUE};
+
+        long[] leftRes = _isValid(root.left);
+        long[] rightRes = _isValid(root.right);
+
+        // 如果左/右子树不是搜索二叉树/root.val比左子树的最大值小/比右子树最小值还大，则不是搜索二叉树
+        if (leftRes[0] == 0 || rightRes[0] == 0 || leftRes[2] >= root.val || rightRes[1] <= root.val) {
+            return new long[] {0, 0, 0};
+        } else {
+            return new long[] {1, Math.min(leftRes[1], root.val), Math.max(rightRes[2], root.val)};
+        }
+    }
     public static void main(String[] args) {
         TreeNodeOperate treeNodeOperate = new TreeNodeOperate();
-        TreeNode root = treeNodeOperate.createTreeByBFS(new Integer[]{5, 4, 6, 3, 7, 3, 7});
+//        TreeNode root = treeNodeOperate.createTreeByBFS(new Integer[]{5, 4, 6, 3, 7, 3, 7});
+        TreeNode root = treeNodeOperate.createTreeByBFS(new Integer[]{2147483647});
         treeNodeOperate.printTree(root);
         System.out.println("------------------------------------");
         LeetCode_S98 solution = new LeetCode_S98();
-        System.out.println(solution.isValidBST(root));
+        System.out.println(solution.isValidBST1(root));
         System.out.println("------------------------------------");
     }
 }
