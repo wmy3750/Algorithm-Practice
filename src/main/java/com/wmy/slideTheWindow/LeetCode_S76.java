12af37a18321ce8773ed6e9c8304dcb50743f14d
wmy <wmyy3750@163.com>
2025-05-06 22:35:54 +0800

    add:LeetCode_S76：最小覆盖子串

---
+++ src/main/java/com/wmy/slideTheWindow/LeetCode_S76.java
@@ -0,0 +1,54 @@
+package com.wmy.slideTheWindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author wangmengyao
+ * @Date 2025/4/15 15:06
+ */
+
+
+public class LeetCode_S76 {
+    public String minWindow(String s, String t) {
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0, right = 0;
+        int start = 0, len = Integer.MAX_VALUE;
+        int valid = 0;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (need.get(c).equals(window.get(c))) {
+                    valid++;
+                }
+            }
+
+            // 难点：valid满足则需要更新窗口
+            while (valid == need.size()) {
+                // 先把子串的起止索引记录
+                if (right - left < len) {
+                    start = left;
+                    len = right - left;
+                }
+
+                char d = s.charAt(left);
+                left++;
+                if (need.containsKey(d)) {
+                    if (need.get(d).equals(window.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.get(d) - 1);
+                }
+            }
+        }
+        return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
+    }
+}
+
