21040ba94ec02893ba4fe894f02ad6e4cfae01e6
wangmengyao <wangmengyao@enneagon.com>
2025-08-01 14:55:35 +0800

    add:热题100之数组相关的滑动窗口 209明显的窗口加减数字  904 开始有map记录种类及其个数了  76经典子串问题,也是map记录种类和次数,这题太经典了多看多记

---
+++ src/main/java/com/wmy/array/slideTheWindow/LeetCode_S904.java
@@ -0,0 +1,28 @@
+package com.wmy.array.slideTheWindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author wangmengyao
+ * @Date 2025/4/21 15:54
+ */
+public class LeetCode_S904 {
+    public int totalFruit(int[] fruits) {
+        int left = 0, right = 0;
+        int res = Integer.MIN_VALUE;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (; right < fruits.length; right++) {
+            map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
+            while (map.size() > 2) {
+                map.put(fruits[left], map.get(fruits[left]) - 1);
+                if (map.get(fruits[left]) == 0) {
+                    map.remove(fruits[left]);
+                }
+                left++;
+            }
+            res = Math.max(res, right - left + 1);
+        }
+        return res;
+    }
+}
