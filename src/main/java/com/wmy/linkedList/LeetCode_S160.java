f094441f22f13e7877223ad1b35307b2c4e5e823
wangmengyao <wangmengyao@enneagon.com>
2025-08-06 19:34:41 +0800

    add:相交链表,想找到相交点,则要p1p2同时遍历两个链表,同时两个节点到头则遍历到对方的节点,遍历结束,则找到是否是相交节点的节点了,

---
+++ src/main/java/com/wmy/linkedList/LeetCode_S160.java
@@ -0,0 +1,19 @@
+package com.wmy.linkedList;
+
+import com.wmy.ListNode;
+
+/**
+ * @author wangmengyao
+ * @Date 2025/4/21 15:54
+ */
+public class LeetCode_S160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA,  p2 = headB;
+
+        while (p1 != p2) {
+            p1 = p1 == null ? headB : p1.next;
+            p2 = p2 == null ? headA : p2.next;
+        }
+        return p1;
+    }
+}
