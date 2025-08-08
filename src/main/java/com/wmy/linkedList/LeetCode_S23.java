cd037cd1e8c7bb1524cff6e52e1b872aee8a3e0d
wangmengyao <wangmengyao@enneagon.com>
2025-08-08 20:07:37 +0800

    add:23合并K个升序链表 也不会 分治思想 能处理两个就能处理K个 关键在于拆分成子问题 同时注意递归终止条件 即只剩一个链表时返回当前链表

---
+++ src/main/java/com/wmy/linkedList/LeetCode_S23.java
@@ -0,0 +1,45 @@
+package com.wmy.linkedList;
+
+import com.wmy.ListNode;
+
+/**
+ * @author wangmengyao
+ * @Date 2025/4/21 15:54
+ */
+public class LeetCode_S23 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    private ListNode merge(ListNode[] lists, int left, int right) {
+        if (left == right) {
+            return lists[left];
+        }
+        int mid = left + ((right - left) >> 1);
+        ListNode l1 = merge(lists, left, mid);
+        ListNode l2 = merge(lists, mid + 1, right);
+        return mergeTwoLists(l1, l2);
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+
+        while(l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+
+        cur.next = l1 == null ? l2 : l1;
+        return dummy.next;
+    }
+}
