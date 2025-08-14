647c98943bebae3592cb380535d46767d930baa0
wangmengyao <wangmengyao@enneagon.com>
2025-08-14 20:25:00 +0800

    add:438.找到字符串中所有字母异位词 有难度的 知道是滑动窗口 但总是写不出来 尤其是边界问题

---
+++ src/main/java/com/wmy/hashtable/LeetCode_S438.java
@@ -0,0 +1,49 @@
+package com.wmy.hashtable;
+
+import java.util.*;
+
+/**
+ * @author wangmengyao
+ * @Date 2025/4/21 15:54
+ */
+public class LeetCode_S438 {
+    public List<Integer> findAnagrams(String s, String p) {
+        if (s.length() < p.length()) {
+            return new ArrayList<>();
+        }
+
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+        for (char c : p.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+        int left = 0, right = 0;
+        int valid = 0;
+        List<Integer> res = new ArrayList<>();
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+            while (right - left >= p.length()) {
+                if (valid == need.size()) {
+                    res.add(left);
+                }
+                char d = s.charAt(left);
+                left++;
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.get(d) - 1);
+                }
+            }
+        }
+        return res;
+    }
+}
