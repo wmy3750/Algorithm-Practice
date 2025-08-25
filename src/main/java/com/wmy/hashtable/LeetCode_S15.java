13a5f543c9744a5b429c5c2167205e122fe1b70e
wangmengyao <wangmengyao@enneagon.com>
2025-08-25 19:28:46 +0800

    add:15三数之和 排序后 先确定一个数字 对后面的范围内找到两个满足要求 不重复三元组 则排序后 遍历下一个等于前一个则跳过 左右边界也是这样跳过 三数之和小了则left++ 大了则right--

---
+++ src/main/java/com/wmy/hashtable/LeetCode_S15.java
@@ -0,0 +1,47 @@
+package com.wmy.hashtable;
+
+import java.util.*;
+
+/**
+ * @author wangmengyao
+ * @Date 2025/4/21 15:54
+ */
+public class LeetCode_S15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
+                    res.add(list);
+
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return res;
+    }
+}
