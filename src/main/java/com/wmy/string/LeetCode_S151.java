1eb3c1b924fa4e86c8c3e032147181124385fd86
wangmengyao <wangmengyao@enneagon.com>
2025-08-25 19:35:50 +0800

    add:151反转字符串中的单词 左右空格先去掉 从后往前遍历 每次左边界走到空格记录一次左到右边界的字符串 然后左边界再走到空格末尾 右边界等于左边界 同上则得到结果

---
+++ src/main/java/com/wmy/string/LeetCode_S151.java
@@ -0,0 +1,27 @@
+package com.wmy.string;
+
+/**
+ * @author wangmengyao
+ * @Date 2025/4/21 15:54
+ */
+public class LeetCode_S151 {
+    public String reverseWords(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        s.trim();
+        int j = s.length() - 1, i = j;
+        StringBuilder res = new StringBuilder();
+        while (i >= 0) {
+            while (i >= 0 && s.charAt(i) != ' ') {
+                i--;
+            }
+            res.append(s.substring(i + 1, j + 1) + " ");
+            while (i >= 0 && s.charAt(i) == ' ') {
+                i--;
+            }
+            j = i;
+        }
+        return res.toString().trim();
+    }
+}
