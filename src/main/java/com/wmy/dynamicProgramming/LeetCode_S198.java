9523ff7740d749f4780ae51db08eb0984509861a
wangmengyao <wangmengyao@enneagon.com>
2025-11-03 19:10:37 +0800

    add:198 打家劫舍I 动态规划简单题

---
+++ src/main/java/com/wmy/dynamicProgramming/LeetCode_S198.java
@@ -0,0 +1,17 @@
+package com.wmy.dynamicProgramming;
+
+/**
+ * @author wangmengyao
+ * @Date 2025/4/21 15:54
+ */
+public class LeetCode_S198 {
+    public int rob(int[] nums) {
+        int dp[] = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 2; i <= nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i  - 2] + nums[i - 1]);
+        }
+        return dp[nums.length];
+    }
+}
