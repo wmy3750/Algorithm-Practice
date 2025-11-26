a6ca902277206521baddf852ce2f6a97e84abd45
wangmengyao <wangmengyao@enneagon.com>
2025-11-26 18:37:42 +0800

    add:139 单词拆分 dp记录状态 必须保证前面能满足且当前j到i的位置被包含

---
+++ src/main/java/com/wmy/dynamicProgramming/LeetCode_S139.java
@@ -0,0 +1,25 @@
+package com.wmy.dynamicProgramming;
+
+import java.util.List;
+
+/**
+ * @author wangmengyao
+ * @Date 2025/4/21 15:54
+ */
+public class LeetCode_S139 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
